55dd64d17a36c4e597f0a5c5546dd4a8c41b04a3
phelimb <wave@phel.im>
2014-08-22 15:55:26 +0100

    Changed lines in genotyping known that were causing FN calls in TB CIP

--- src/predictor/core/genotyping_known.c
+++ src/predictor/core/genotyping_known.c
@@ -398,10 +398,13 @@ void get_next_mutation_allele_info(FILE* fp, dBGraph* db_graph, ResVarInfo* rinf
 
   if (rinfo->var_id!= *prev_mut)
     {
-      reset_res_var_info(rinfo);
-      //rinfo->working_current_max_sus_allele_present=0;
-      //rinfo->working_current_max_res_allele_present=0;
-      *prev_mut=km; //for use in the next call to this function
+    	// This makes S CIP calls int TB and C00007102,C00009008 fusidic as S  (should be S) and R (should be R) 
+      // reset_res_var_info(rinfo);
+      // *prev_mut=km; //for use in the next call to this function
+      // This makes R CIP calls in TB and C00007102,C00009008 fusidic as r (should be S) and r (should be R)
+      *prev_mut=rinfo->var_id; //for use in the next call to this function
+      rinfo->working_current_max_sus_allele_present=0;
+      rinfo->working_current_max_res_allele_present=0;
     }
   rinfo->gene=g;
   rinfo->var_id=km;
