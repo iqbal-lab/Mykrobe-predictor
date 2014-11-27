3f5cf11a10b9a1b1ace4e47251d116c6d3c32984
Zam Iqbal (on laptop) <zam@well.ox.ac.uk>
2014-11-27 16:32:48 +0000

    Remove references to GPLv3, accidentally in there. Add license file.

--- src/predictor/tb/species.c
+++ src/predictor/tb/species.c
@@ -1,27 +1,6 @@
 /*
  * Copyright 2014 Zamin Iqbal (zam@well.ox.ac.uk)
- * 
  *
- * **********************************************************************
- *
- * This file is part of Mykrobe.
- *
- * Mykrobe is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Mykrobe is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Mykrobe.  If not, see <http://www.gnu.org/licenses/>.
- *
- * **********************************************************************
- */
-/*
   species.c
 */
 
