de64f55c3ea5f61595d70bcd6363d2d8357436cf
phelimb <wave@phel.im>
2015-03-12 15:18:13 +0000

    closes #48

--- src/test/myKrobe/predictor/test_species_prediction.c
+++ src/test/myKrobe/predictor/test_species_prediction.c
@@ -2,24 +2,8 @@
  * Copyrightf 2015 Zamin Iqbal (zam@well.ox.ac.uk)
  * 
  *
- * **********************************************************************
+ * Copyright 2015 Zamin Iqbal (zam@well.ox.ac.uk)
  *
- * This file is part of myKrobe.
- *
- * myKrobe is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * myKrobe is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with myKrobe.  If not, see <http://www.gnu.org/licenses/>.
- *
- * **********************************************************************
  */
 /*
   test_species_prediction.c 
