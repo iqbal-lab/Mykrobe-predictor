3caef27372e619302f948b2ac744be94ef0b9e61
Phelim Bradley <wave@phel.im>
2015-03-24 12:36:11 +0000

    moved seqfile to https://github.com/Phelimb/seq_file

--- libs/seq_file/build_win.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-make CC=gcc HTS_PATH=../htslib/ STRING_BUF_PATH=../string_buffer/
-
-# need tools/new_api/Makefile fix
-
--- libs/seq_file/new_api/tools/build_win.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-make CC=gcc HTSLIB=../../../htslib/
-
-# STRING_BUF_PATH=../../../string_buffer/
-
-
