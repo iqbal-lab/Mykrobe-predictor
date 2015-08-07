8ff1a77bf1a6d9452738c369aa64e3215456bf08
Phelim Bradley <wave@phel.im>
2015-08-07 13:11:20 +0100

    Rearranged species data into sub folders by complex

--- make.sh
+++ make.sh
@@ -1,19 +1,44 @@
-sh render.sh
+make clean
 rm data/skeleton_binary/*/skeleton.k15.ctx
+
+cd code_generator
+python staph.py
+cd ..
+cp code_generator/rendered/include/predictor/core/* include/predictor/core/
+cp code_generator/rendered/include/predictor/staph/* include/predictor/staph/
+cp code_generator/rendered/src/predictor/core/* src/predictor/core/
+cp code_generator/rendered/src/predictor/staph/* src/predictor/staph/
 
 cd data/skeleton_binary/staph/
 ls ../../../data/staph/*/*.fa > list_speciesbranches_genes_and_muts
 ls ../../../data/staph/*/*.fasta >> list_speciesbranches_genes_and_muts
+ls ../../../data/staph/*/*/*.fasta >> list_speciesbranches_genes_and_muts
+ls ../../../data/staph/*/*/*.fasta >> list_speciesbranches_genes_and_muts
+cd ../../../
 
-cd ../../../
+make STAPH=1 predictor
+cd bin
+./Mykrobe.predictor.staph  --file ../data/staph/phylo/species/Saureus.fasta --install_dir ../
+cd ..
+
+
+cd code_generator
+python tb.py
+cd ..
+
 cd data/skeleton_binary/tb/
-ls ../../../data/tb/*/*.fa > list_speciesbranches_genes_and_muts
-ls ../../../data/tb/*/*.fasta >> list_speciesbranches_genes_and_muts
+ls ../../tb/antibiotics/*.fa > list_speciesbranches_genes_and_muts
+ls ../../tb/virulence/*.fa >> list_speciesbranches_genes_and_muts
+ls ../../tb/phylo/*/*.fa >> list_speciesbranches_genes_and_muts
 cd ../../../
+
+
+cp code_generator/rendered/include/predictor/core/* include/predictor/core/
+cp code_generator/rendered/include/predictor/tb/* include/predictor/tb/
+cp code_generator/rendered/src/predictor/core/* src/predictor/core/
+cp code_generator/rendered/src/predictor/tb/* src/predictor/tb/
 
 make TB=1 predictor 
-make STAPH=1 predictor
-
 cd bin
-./Mykrobe.predictor.tb  --file ../data/tb/species/MTBC.fa --install_dir ../
-./Mykrobe.predictor.staph  --file ../data/staph/species/Saureus.fasta --install_dir ../
+./Mykrobe.predictor.tb  --file ../data/tb/phylo/complex/MTBC.fa --install_dir ../ --verbose
+cd ..
