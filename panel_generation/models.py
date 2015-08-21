bb13bc480b99a79cab88f16c4bcbec2592713ac0
phelimb <wave@phel.im>
2015-08-21 16:30:51 +0100

    Added more complex context situations

--- panel_generation/models.py
+++ panel_generation/models.py
@@ -1,6 +1,21 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
 from copy import copy 
+import itertools
+from collections import Counter
+
+def unique(seq):
+    seen = set()
+    seen_add = seen.add
+    return [ x for x in seq if x not in seen and not seen_add(x)]
+
+class Variant(object):
+
+	def __init__(self, ref, pos, alt):
+		self.ref = ref 
+		self.pos = pos
+		self.alt = alt
+
 class AlleleGenerator(object):
 	"""docstring for PanelGenerator"""
 	def __init__(self, reference_filepath, kmer = 31):
@@ -15,20 +30,73 @@ class AlleleGenerator(object):
 			self.ref += list(record.seq)
 		self.ref_length = len(self.ref)
 
-	def create(self, ref, pos, alt):
+	def create(self, v, context = []):
 		## Position should be 1 based
-		if pos < 1:
-			pos = self.ref_length + pos
-		assert pos > 0
-		index = pos - 1
-		if self.ref[index] != ref:
-			raise ValueError("Cannot create alleles as ref at pos %i is not %s (it's %s) are you sure you're using one-based co-ordinates?" % (pos, ref, self.ref[index]))
-		i, start_index, end_index = self._get_start_end(pos)
-		reference = self.ref[start_index:end_index]
-		alternate = copy(reference)
-		alternate[i] = alt
-		return Panel(reference, pos, alternate)	
+		assert v.pos > 0
+		i, start_index, end_index = self._get_start_end(v.pos)
+		self._check_valid_variant(v)
+		wild_type_reference = copy(self.ref[start_index:end_index])
+		backgrounds = self._generate_all_backgrounds_using_context(v, i, wild_type_reference, context)
+		alternates = self._generate_alternates_on_all_backgrounds(i, v,  backgrounds)
+		return Panel(wild_type_reference, v.pos, alternates)
 
+	def _generate_alternates_on_all_backgrounds(self, i, v, backgrounds):
+		alternates = []
+		for background in backgrounds:
+			alternate = copy(background)
+			alternate[i] = v.alt
+			alternates.append(alternate)
+		return alternates
+		
+	def _check_valid_variant(self, v):
+		index = v.pos - 1
+		if self.ref[index] != v.ref:
+			raise ValueError("""Cannot create alleles as ref at pos %i is not %s (it's %s) are you sure you're using one-based co-ordinates?
+							 """ % (v.pos, v.ref, self.ref[index]))			
+
+	def _generate_all_backgrounds_using_context(self, v, i, wild_type_reference, context = []):
+		backgrounds = [wild_type_reference]
+		if context:
+			contexts = self._create_multiple_contexts(context)
+			for context in contexts:
+				context_combinations = self._get_combinations_of_backgrounds(context)
+				for variants in context_combinations:
+					new_background = copy(wild_type_reference)
+					for variant in variants:
+						j = i + variant.pos - v.pos
+						new_background[j] = variant.alt
+					backgrounds.append(new_background)
+		return backgrounds
+
+	def _create_multiple_contexts(self, context):
+		new_contexts = self._recursive_context_creator([context])
+		return new_contexts
+
+	def _recursive_context_creator(self, contexts):
+		for i,context in enumerate(contexts):
+			position_counts = Counter([v.pos for v in context])
+			if not all([c == 1 for c in position_counts.values()]):
+				position = position_counts.most_common(1)[0][0]
+				repeated_vars = []
+				common_vars = []
+				for var in context:
+					if var.pos == position:
+						repeated_vars.append(var)
+					else:
+						common_vars.append(var)
+				new_contexts = []
+				for var in repeated_vars:
+					context = [var] + common_vars
+					new_contexts.append(context)
+				self._recursive_context_creator(new_contexts)				
+		return contexts
+
+	def _get_combinations_of_backgrounds(self, context):
+		combination_context = []
+		for L in range(1, len(context)+1):
+		  for subset in itertools.combinations(context, L):
+		    combination_context.append(subset)	
+		return combination_context
 
 	def _get_start_end(self, pos):
 		start_index = pos - self.kmer
@@ -49,9 +117,9 @@ class AlleleGenerator(object):
 
 class Panel(object):
 
-	def __init__(self, ref, pos, alt):
+	def __init__(self, ref, pos, alts):
 		self.ref = "".join(ref)
 		self.pos = pos
-		self.alt = "".join(alt)
+		self.alts = unique(["".join(alt) for alt in alts])
 
 		
--- panel_generation/test.py
+++ panel_generation/test.py
@@ -1,4 +1,5 @@
 from models import AlleleGenerator 
+from models import Variant 
 from nose.tools import assert_raises
 
 class BaseTest():
@@ -7,35 +8,93 @@ class BaseTest():
         pass
         
     def teardown(self):
-    	pass
+        pass
 
-class TestReference(BaseTest):
+class TestSNPAlleleGenerator(BaseTest):
 
     def setUp(self):
-    	self.pg = AlleleGenerator(reference_filepath = "/home/phelimb/git/atlas/data/R00000022.fasta")
+        self.pg = AlleleGenerator(reference_filepath = "/home/phelimb/git/atlas/data/R00000022.fasta")
 
-    def test_panel_generator(self):
-    	pg = AlleleGenerator(reference_filepath = "/home/phelimb/git/atlas/data/R00000022.fasta")
-    	assert pg.ref is not None
+    # def test_panel_generator(self):
+    #     pg = AlleleGenerator(reference_filepath = "/home/phelimb/git/atlas/data/R00000022.fasta")
+    #     assert pg.ref is not None
 
-    def test_simple_variant(self):
-    	panel = self.pg.create("A", 31, "T")
-    	assert str(panel.ref) == "CGATTAAAGATAGAAATACACGATGCGAGCAATCAAATTTCATAACATCACCATGAGTTTGAT"
-    	assert str(panel.alt) == "CGATTAAAGATAGAAATACACGATGCGAGCTATCAAATTTCATAACATCACCATGAGTTTGAT"
+    # def test_simple_variant(self):
+    #     v = Variant("A", 31, "T")
+    #     panel = self.pg.create(v)
+    #     assert panel.ref ==   "CGATTAAAGATAGAAATACACGATGCGAGCAATCAAATTTCATAACATCACCATGAGTTTGAT"
+    #     assert panel.alts == ["CGATTAAAGATAGAAATACACGATGCGAGCTATCAAATTTCATAACATCACCATGAGTTTGAT"]
 
-    def test_simple_variant_invalid(self):
-    	with assert_raises(ValueError) as cm:
-    		panel = self.pg.create("T", 31, "T")
+    #     v = Variant("A", 32, "T")
+    #     panel = self.pg.create(v)
+    #     assert panel.ref ==   "GATTAAAGATAGAAATACACGATGCGAGCAATCAAATTTCATAACATCACCATGAGTTTGATC"
+    #     assert panel.alts == ["GATTAAAGATAGAAATACACGATGCGAGCATTCAAATTTCATAACATCACCATGAGTTTGATC"]        
 
-    def test_simple_variant_start(self):
-    	panel = self.pg.create("C", 1, "T")
-    	assert str(panel.ref) == "CGATTAAAGATAGAAATACACGATGCGAGCAATCAAATTTCATAACATCACCATGAGTTTGAT"
-    	assert str(panel.alt) == "TGATTAAAGATAGAAATACACGATGCGAGCAATCAAATTTCATAACATCACCATGAGTTTGAT"    
+    # def test_simple_variant_invalid(self):
+    #     with assert_raises(ValueError) as cm:
+    #         v = Variant("T", 31, "T")
+    #         panel = self.pg.create(v)
 
-    def test_simple_variant_end(self):
-    	panel = self.pg.create("A", 2902618, "T")
-    	assert str(panel.ref) == "TAACAAAATCCTTTTTATAACGCAAGTTCATTTTATACTACTGCTCAATTTTTTTACTTTTAT"
-    	assert str(panel.alt) == "TAACAAAATCCTTTTTATAACGCAAGTTCATTTTATACTACTGCTCAATTTTTTTACTTTTTT"        			
-        panel = self.pg.create("A", -1, "T")
-        assert str(panel.ref) == "TAACAAAATCCTTTTTATAACGCAAGTTCATTTTATACTACTGCTCAATTTTTTTACTTTTAT"
-        assert str(panel.alt) == "TAACAAAATCCTTTTTATAACGCAAGTTCATTTTATACTACTGCTCAATTTTTTTACTTTTTT" 
+    # def test_simple_variant_start(self):
+    #     v = Variant("C", 1, "T")
+    #     panel = self.pg.create(v)
+    #     assert panel.ref == "CGATTAAAGATAGAAATACACGATGCGAGCAATCAAATTTCATAACATCACCATGAGTTTGAT"
+    #     assert panel.alts == ["TGATTAAAGATAGAAATACACGATGCGAGCAATCAAATTTCATAACATCACCATGAGTTTGAT"]
+
+    # def test_simple_variant_end(self):
+    #     v = Variant("A", 2902618, "T")     
+    #     panel = self.pg.create(v)
+    #     assert panel.ref ==   "TAACAAAATCCTTTTTATAACGCAAGTTCATTTTATACTACTGCTCAATTTTTTTACTTTTAT"
+    #     assert panel.alts == ["TAACAAAATCCTTTTTATAACGCAAGTTCATTTTATACTACTGCTCAATTTTTTTACTTTTTT"]
+
+    #     v = Variant("T", 2902616, "C")
+    #     panel = self.pg.create(v)
+    #     assert panel.ref ==   "TAACAAAATCCTTTTTATAACGCAAGTTCATTTTATACTACTGCTCAATTTTTTTACTTTTAT"
+    #     assert panel.alts == ["TAACAAAATCCTTTTTATAACGCAAGTTCATTTTATACTACTGCTCAATTTTTTTACTTCTAT"]
+
+    # def test_simple_variant_with_nearby_snp(self):
+    #     v = Variant("A", 31, "T")
+    #     v2 = Variant("A", 32, "T")
+    #     panel = self.pg.create(v, context = [v2])
+    #     assert panel.ref ==   "CGATTAAAGATAGAAATACACGATGCGAGCAATCAAATTTCATAACATCACCATGAGTTTGAT"
+    #     assert panel.alts == ["CGATTAAAGATAGAAATACACGATGCGAGCTATCAAATTTCATAACATCACCATGAGTTTGAT",
+    #                           "CGATTAAAGATAGAAATACACGATGCGAGCTTTCAAATTTCATAACATCACCATGAGTTTGAT"]    
+
+    # def test_simple_variant_with_multiple_nearby_snps(self):
+    #     v = Variant("A", 31, "T")
+    #     v2 = Variant("A", 32, "T")
+    #     v3 = Variant("C", 30, "G")
+
+    #     panel = self.pg.create(v, context = [v2, v3])
+    #     print panel.alts
+    #     assert panel.ref ==   "CGATTAAAGATAGAAATACACGATGCGAGCAATCAAATTTCATAACATCACCATGAGTTTGAT"
+    #     assert panel.alts == ["CGATTAAAGATAGAAATACACGATGCGAGCTATCAAATTTCATAACATCACCATGAGTTTGAT",
+    #                           "CGATTAAAGATAGAAATACACGATGCGAGCTTTCAAATTTCATAACATCACCATGAGTTTGAT",
+    #                           "CGATTAAAGATAGAAATACACGATGCGAGGTATCAAATTTCATAACATCACCATGAGTTTGAT",
+    #                           "CGATTAAAGATAGAAATACACGATGCGAGGTTTCAAATTTCATAACATCACCATGAGTTTGAT"]
+
+    # def test_multiple_contexts(self):
+    #     self.pg._create_multiple_contexts([v2, v3, v4, v5])
+
+    def test_simple_variant_with_multiple_nearby_snps2(self):
+        v = Variant("A", 31, "T")
+        v2 = Variant("A", 32, "T")
+        v3 = Variant("C", 30, "G")
+        v4 = Variant("C", 30, "T")
+        v5 = Variant("C", 30, "A")
+
+        panel = self.pg.create(v, context = [v2, v3, v4, v5])
+        print panel.alts
+        assert panel.ref ==   "CGATTAAAGATAGAAATACACGATGCGAGCAATCAAATTTCATAACATCACCATGAGTTTGAT"
+        assert sorted(panel.alts) == sorted(["CGATTAAAGATAGAAATACACGATGCGAGCTATCAAATTTCATAACATCACCATGAGTTTGAT",
+                              "CGATTAAAGATAGAAATACACGATGCGAGCTTTCAAATTTCATAACATCACCATGAGTTTGAT",
+                              "CGATTAAAGATAGAAATACACGATGCGAGGTATCAAATTTCATAACATCACCATGAGTTTGAT",
+                              "CGATTAAAGATAGAAATACACGATGCGAGGTTTCAAATTTCATAACATCACCATGAGTTTGAT",
+                              "CGATTAAAGATAGAAATACACGATGCGAGTTATCAAATTTCATAACATCACCATGAGTTTGAT",
+                              "CGATTAAAGATAGAAATACACGATGCGAGTTTTCAAATTTCATAACATCACCATGAGTTTGAT",
+                              "CGATTAAAGATAGAAATACACGATGCGAGATATCAAATTTCATAACATCACCATGAGTTTGAT",
+                              "CGATTAAAGATAGAAATACACGATGCGAGATTTCAAATTTCATAACATCACCATGAGTTTGAT"])
+
+
+
+
