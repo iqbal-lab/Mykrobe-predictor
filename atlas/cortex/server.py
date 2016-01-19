86648362b84fdf708fe9743b2c343af095f88fa0
Phelim Bradley <wave@phel.im>
2016-01-19 20:11:01 +0000

    Removed unnessecary logging and have working test case

--- atlas/cortex/server.py
+++ atlas/cortex/server.py
@@ -17,10 +17,19 @@ from subprocess import Popen, PIPE
 from http.server import BaseHTTPRequestHandler
 import socketserver
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+from pprint import pprint
+import copy
+logging.getLogger('requests.packages.urllib3.util').setLevel(logging.WARNING)
+logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(logging.WARNING)
+logging.getLogger('requests.packages').setLevel(logging.WARNING)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
+logging.getLogger('requests.packages.urllib3').setLevel(logging.WARNING)
+logging.getLogger('requests.packages.urllib3.util.retry').setLevel(logging.WARNING)
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('requests.packages.urllib3.poolmanager').setLevel(logging.WARNING)
 
-#
 # Start server on port 2306, with link and graph files:
 #   python mccortex-server.py 2306 --coverages --edges -p l.ctp.gz a.ctx b.ctx
 # Query a kmer:
@@ -63,6 +72,10 @@ class WebServer(object):
         mccortex = self.mccortex
         class McCortexHTTPServer(BaseHTTPRequestHandler):
 
+            def log_message(self, format, *args):
+                ## Remove logging
+                pass
+
             def do_GET(self):
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
@@ -71,7 +84,10 @@ class WebServer(object):
                     jsonstr = "{\"error\": \"Webserver: bad query\"}\n"
                 else:
                     jsonstr = query_mccortex(mccortex, self.path[1:])
-                self.wfile.write(jsonstr.encode("UTF-8"))        
+                try:
+                    self.wfile.write(jsonstr.encode("UTF-8"))        
+                except UnicodeDecodeError:
+                    self.wfile.write("{}".encode("UTF-8"))
         try:
             self.httpd = socketserver.TCPServer( ("", self.port), McCortexHTTPServer)
         except Exception as e:
@@ -113,19 +129,18 @@ class WebServer(object):
 
 class McCortexQuery(object):
 
-    def __init__(self, port, seeds = []):
+    def __init__(self, port):
           self.base_url = "http://localhost:%i/" % port
-          self.seeds = seeds
 
-    def query(self, kmer):
-        return McCortexQueryResult(kmer, requests.get(self.base_url + kmer).json(), seeds = self.seeds)
+    def query(self, kmer, known_kmers = []):
+        return McCortexQueryResult(kmer, requests.get(self.base_url + kmer).json(), known_kmers = known_kmers)
 
 class McCortexQueryResult(object):
 
-    def __init__(self, query_kmer, data, seeds = []):
+    def __init__(self, query_kmer, data, known_kmers = []):
       self.query_kmer = query_kmer
       self.data = data
-      self.seeds = seeds
+      self.known_kmers = known_kmers
 
     def __str__(self):
       return str(self.data)
@@ -141,8 +156,9 @@ class McCortexQueryResult(object):
       else:
           for r in self.right:
             forward_kmers.append(str(self.data["key"][1:] + r))
-      if len(forward_kmers) > 1:
-          forward_kmers = [f for f in forward_kmers if f in self.seeds]
+      if len(forward_kmers) > 1 and self.known_kmers:
+          forward_kmers = [f for f in forward_kmers if f in self.known_kmers]
+      assert len(forward_kmers) > 0
       return forward_kmers
 
     @property
@@ -169,6 +185,65 @@ class McCortexQueryResult(object):
     @property
     def complement(self):
       return self.kmer != self.query_kmer
+
+class GraphWalker(object):
+
+    def __init__(self, port, kmer_size = 31):
+        self.mcq =  McCortexQuery(port)
+        self.queries = {}
+        self.kmer_size = kmer_size
+
+    def breath_first_search(self, N, seed, end_kmers = [], known_kmers = []):
+        paths = {0 : { "dna" : seed[:self.kmer_size]}}
+        for _ in range(1000):
+            for i in paths.keys():
+                if not paths[i]["dna"][-1] == "*":
+                    k = paths[i]["dna"][-self.kmer_size:]
+                    if k in end_kmers:
+                         paths[i]["dna"] = paths[i]["dna"] + "*"
+                         k = paths[i]["dna"][-self.kmer_size:]
+                    try:
+                        q = self.queries[k]
+                    except KeyError: 
+                        try:
+                            q =  self.mcq.query(k, known_kmers = known_kmers)
+                            data = q.data
+                            self.queries[k] = q
+                        except ValueError:
+                            data = {}
+                            self.queries[k] = None
+                            paths[i]["dna"] = paths[i]["dna"] + "*"
+                    if data.get("key") and q is not None and q.data.get("key"):
+                        kmers = q.forward()
+                        if len(kmers) > 1:
+                            kmers = [k for k in kmers if self.mcq.query(k, known_kmers = known_kmers).depth > (q.depth * 0.1)]
+                        if len(kmers) < 1:
+                            paths[i]["dna"] = paths[i]["dna"] + "*"
+                        ## Create new paths
+                        paths = self.create_new_paths(paths, i, len(kmers) - 1)
+                        for j, kmer in enumerate(kmers):
+                            if j == 0:
+                                paths[i]["dna"] = paths[i]["dna"] + kmer[-1]
+                                if len(paths[i]["dna"]) % 3:
+                                     paths[i]["prot"] = str(Seq(paths[i]["dna"]).translate(11))
+                            else:
+                                paths[sorted(paths.keys())[-j]]["dna"] = paths[sorted(paths.keys())[-j]]["dna"] + kmer[-1]
+            paths[i]["len_dna"] = len(paths[i]["dna"])
+            paths[i]["prot"] = str(Seq(paths[i]["dna"].replace("*","")).translate(11))            
+            paths[i]["len_prot"] = len(paths[i]["prot"])
+        keep_paths = {}
+        for k,v in paths.items():
+            if v["len_dna"] == N + 1:
+                 keep_paths[k] = v
+        return keep_paths.values()
+
+    def create_new_paths(self,paths, i, num):
+        num_paths = max(paths.keys())
+        for j in range(num):
+            paths[num_paths + j + 1] = copy.copy(paths[i])
+        return paths
+
+
 
 # def main():
 #     if len(sys.argv) < 3 or not sys.argv[1].isdigit():
