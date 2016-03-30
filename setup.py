5bbeb50213297e72e33b57fed7e30b8ef09a6e03
phelimb <wave@phel.im>
2016-03-30 18:12:11 +0100

    Update setup.py data

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
             'data/predict/*/*',
             'data/predict/taxon_coverage_threshold.json',
             'data/phylo/*',
-            'data/panels/tb-species-160227.fasta.gz',
+            'data/panels/tb-species-160330.fasta.gz',
             'data/panels/staph-species-160227.fasta.gz',
             'data/panels/tb-amr-walker_2015.fasta.gz',
             'data/panels/tb-amr-bradley_2015.fasta.gz',
